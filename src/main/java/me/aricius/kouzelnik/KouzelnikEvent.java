dc275ac09508ed5ca12c73ba3808f187ac902d2d
MonsignorAricius <aricius@centrum.sk>
2024-03-07 21:11:39 +0100

    Armor dupe fix

--- src/main/java/me/aricius/kouzelnik/KouzelnikEvent.java
+++ src/main/java/me/aricius/kouzelnik/KouzelnikEvent.java
@@ -1,22 +1,29 @@
 package me.aricius.kouzelnik;
 
 import net.md_5.bungee.api.ChatColor;
+import org.bukkit.Bukkit;
 import org.bukkit.Material;
 import org.bukkit.enchantments.Enchantment;
 import org.bukkit.entity.Entity;
+import org.bukkit.entity.Item;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.player.PlayerInteractEntityEvent;
+import org.bukkit.event.player.PlayerInteractEvent;
 import org.bukkit.inventory.EquipmentSlot;
 import org.bukkit.inventory.Inventory;
 import org.bukkit.inventory.ItemStack;
 import org.bukkit.plugin.Plugin;
 
 import java.util.Arrays;
+import java.util.HashMap;
+import java.util.UUID;
 
 public class KouzelnikEvent implements Listener {
     private final Plugin plugin = Kouzelnik.getPlugin(Kouzelnik.class);
+
+    private final HashMap<UUID, ItemStack> itemStorage = new HashMap<>();
     
     @EventHandler
     public void onPlayerClickNPC(PlayerInteractEntityEvent e) {
@@ -24,7 +31,64 @@ public class KouzelnikEvent implements Listener {
         Entity npc = e.getRightClicked();
         if (e.getHand() == EquipmentSlot.HAND) {
             if (npc.hasMetadata("NPC") && npc.getName().equals("Kouzelník")) {
-                openKouzelnik(player);
+                ItemStack oldHand = player.getInventory().getItemInMainHand();
+                String itemis = oldHand.getType().name();
+                if (itemis.endsWith("_HELMET")
+                        || itemis.endsWith("_CHESTPLATE")
+                        || itemis.endsWith("_LEGGINGS")
+                        || itemis.endsWith("_BOOTS")) {
+                    itemStorage.put(player.getUniqueId(), oldHand.clone());
+                    ItemStack helm = player.getInventory().getHelmet();
+                    ItemStack ches = player.getInventory().getChestplate();
+                    ItemStack legg = player.getInventory().getLeggings();
+                    ItemStack boot = player.getInventory().getBoots();
+                    if (helm != null) {
+                        player.sendMessage("§8["+ChatColor.of("#FB608A")+"§lKouzelník§8]"+" §7Sundej si prosím helmu.");
+                        itemStorage.remove(player.getUniqueId());
+                        return;
+                    }
+                    if (ches != null) {
+                        player.sendMessage("§8["+ChatColor.of("#FB608A")+"§lKouzelník§8]"+" §7Sundej si prosím chestplate.");
+                        itemStorage.remove(player.getUniqueId());
+                        return;
+                    }
+                    if (legg != null) {
+                        player.sendMessage("§8["+ChatColor.of("#FB608A")+"§lKouzelník§8]"+" §7Sundej si prosím tepláky.");
+                        itemStorage.remove(player.getUniqueId());
+                        return;
+                    }
+                    if (boot != null) {
+                        player.sendMessage("§8["+ChatColor.of("#FB608A")+"§lKouzelník§8]"+" §7Sundej si prosím boty.");
+                        itemStorage.remove(player.getUniqueId());
+                        return;
+                    }
+                    openKouzelnik(player);
+                    Bukkit.getScheduler().runTaskLater(plugin, () -> {
+                        Material hashedItem = itemStorage.get(player.getUniqueId()).getType();
+                        if (hashedItem.name().endsWith("_HELMET")) {
+                            player.getInventory().setHelmet(new ItemStack(Material.AIR));
+                            player.getInventory().setItemInMainHand(itemStorage.get(player.getUniqueId()));
+                            itemStorage.remove(player.getUniqueId());
+                        }
+                        if (hashedItem.name().endsWith("_CHESTPLATE")) {
+                            player.getInventory().setChestplate(new ItemStack(Material.AIR));
+                            player.getInventory().setItemInMainHand(itemStorage.get(player.getUniqueId()));
+                            itemStorage.remove(player.getUniqueId());
+                        }
+                        if (hashedItem.name().endsWith("_LEGGINGS")) {
+                            player.getInventory().setLeggings(new ItemStack(Material.AIR));
+                            player.getInventory().setItemInMainHand(itemStorage.get(player.getUniqueId()));
+                            itemStorage.remove(player.getUniqueId());
+                        }
+                        if (hashedItem.name().endsWith("_BOOTS")) {
+                            player.getInventory().setBoots(new ItemStack(Material.AIR));
+                            player.getInventory().setItemInMainHand(itemStorage.get(player.getUniqueId()));
+                            itemStorage.remove(player.getUniqueId());
+                        }
+                    }, 5L);
+                } else {
+                    openKouzelnik(player);
+                }
             }
         }
     }
